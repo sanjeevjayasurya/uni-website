81e61d23350d08d9a6c4e10adcac0710fd2df9ce
sanjeevjayasurya <sanjeev11398@gmail.com>
2021-10-19 01:41:59 +0530

    padding change

--- components/HeroTemplateHorizontal.js
+++ components/HeroTemplateHorizontal.js
@@ -1,5 +1,5 @@
-import { heroTemplate } from 'contents/heroTemplate'
-import Image from './Image'
+import { heroTemplate } from "contents/heroTemplate";
+import Image from "./Image";
 
 const {
   titleFirstLine,
@@ -8,13 +8,18 @@ const {
   download,
   imageSrcHorizontal,
   imageAlt,
-} = heroTemplate
+} = heroTemplate;
 
 export const HeroTemplateHorizontal = () => {
   return (
     <main className="relative text-left bg-black">
       <div>
         <div className="items-center text-center md:text-left max-w-[1600px] mx-auto h-screen w-full flex flex-col justify-end md:justify-around md:items-center md:flex-row sm:px-8 md:px-14 pb-40 md:pt-40">
+          <Image
+            className="bottom-0 w-60 h-60 md:w-80 md:h-80"
+            src={imageSrcHorizontal}
+            alt={imageAlt}
+          ></Image>
           <div className="flex items-center flex-col mt-20 items-left md:justify-center md:items-start md:max-w-[500px] md:mt-0">
             <div className="text-5xl md:text-6xl lg:text-7xl max-w-[350px] md:max-w-full text-white font-bold mb-6 flex-wrap">
               {titleFirstLine}
@@ -30,13 +35,8 @@ export const HeroTemplateHorizontal = () => {
               {download}
             </a>
           </div>
-          <Image
-            className="bottom-0 w-60 h-60 md:w-80 md:h-80"
-            src={imageSrcHorizontal}
-            alt={imageAlt}
-          ></Image>
         </div>
       </div>
     </main>
-  )
-}
+  );
+};
