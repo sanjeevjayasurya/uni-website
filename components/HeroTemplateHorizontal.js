4f62971f262886ccfb8cf8a69ab4340c9b3df4f2
sanjeevjayasurya <sanjeev11398@gmail.com>
2021-10-19 02:14:53 +0530

    change justify styles

--- components/HeroTemplateHorizontal.js
+++ components/HeroTemplateHorizontal.js
@@ -14,7 +14,7 @@ export const HeroTemplateHorizontal = () => {
   return (
     <main className="relative text-left bg-black">
       <div>
-        <div className="items-center text-center md:text-left max-w-[1600px] mx-auto h-screen w-full flex flex-col justify-end md:justify-around md:items-center md:flex-row sm:px-8 md:px-14 pb-16">
+        <div className="items-center text-center md:text-left max-w-[1600px] mx-auto h-screen w-full flex flex-col justify-center md:justify-around md:items-center md:flex-row sm:px-8 md:px-14 pb-16">
           <Image
             className="bottom-0 w-60 h-60 md:w-80 md:h-80"
             src={imageSrcHorizontal}
