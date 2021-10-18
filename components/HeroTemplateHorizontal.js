74bed2da9f893b79503fe22e4c1e5c56334d1378
Sanjeev <sanjeev@SanjeevMac1.local>
2021-10-19 00:59:49 +0530

    change herotemplatehorizontal

--- components/HeroTemplateHorizontal.js
+++ components/HeroTemplateHorizontal.js
@@ -14,8 +14,8 @@ export const HeroTemplateHorizontal = () => {
   return (
     <main className="relative text-left bg-black">
       <div>
-        <div className="items-center text-center md:text-left max-w-[1600px] mx-auto h-screen w-full flex flex-col justify-end md:justify-around md:items-center md:flex-row px-8 pb-40 md:pt-40 sm:px-14">
-          <div className="flex items-center flex-col mt-16 items-left md:justify-center md:items-start md:max-w-[500px] md:mt-0">
+        <div className="items-center text-center md:text-left max-w-[1600px] mx-auto h-screen w-full flex flex-col justify-end md:justify-around md:items-center md:flex-row md:px-8 pb-40 md:pt-40 sm:px-14">
+          <div className="flex items-center flex-col mt-20 items-left md:justify-center md:items-start md:max-w-[500px] md:mt-0">
             <div className="text-5xl md:text-6xl lg:text-7xl max-w-[350px] md:max-w-full text-white font-bold mb-6 flex-wrap">
               {titleFirstLine}
               <span className="block md:inline">{titleSecondLine}</span>
