75e77b6cbc30691b4d05b14c219b29f5c733d3b8
sanjeevjayasurya <sanjeev11398@gmail.com>
2021-10-19 01:34:40 +0530

    padding change

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/orion-two":["static\u002Fchunks\u002Fpages\u002Forion-two.js"],sortedPages:["\u002F_app","\u002F_error","\u002Forion-two"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
